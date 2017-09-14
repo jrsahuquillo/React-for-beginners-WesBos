5274dfbef18680c7741c62b231f22cf8252aa50a
jrsahuquillo <jrsahuquillo@gmail.com>
2017-09-15 00:12:26 +0200

    Loads CSS into React Application

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,7 +4,6 @@ class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
-      { /* Comments */}
         <h2>Please Enter a Store</h2>
         <input type="text" required placeholder="Store Name" />
         <button type="submit">Visit Store >></button>
