a4322c2525e76d3881ad2189a9ed654c828de9ec
jrsahuquillo <jrsahuquillo@gmail.com>
2017-09-15 18:37:39 +0200

    Sets goToStore React Event

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  goToStore(event) {
+  	event.preventDefault();
+  	console.log('Patatafrito');
+  	//First grab the text from the box
+  	console.log(this.storeInput.value);
+  	//Second we're going to transition from / to /store/:storeiId
+  }
+
   render() {
+  	//Any where else
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store >></button>
       </form>
     )
