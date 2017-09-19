c53452d4ec819947a0e6e49f1288576352e32ab0
jrsahuquillo <jrsahuquillo@gmail.com>
2017-09-19 09:23:19 +0200

    Sets state persistence with Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase  from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDgoq1KEmPHswdGl0ikh-qU7UuuOYId1xU",
+  authDomain: "catch-of-the-day-wesbos.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-wesbos.firebaseio.com",
+});
+
+export default base;
